4cdbe11ed6746979116011968ba996ee69fbf37d
alex ora <alex117a@hotmail.com>
2020-11-04 11:01:13 -0600

    Se añadio el contador a la aplicacion

---
+++ lib/src/pages/contador_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ContadorPage extends StatefulWidget {
+  @override
+  createState() {
+    return _ContadorPageState();
+  }
+}
+
+class _ContadorPageState extends State<ContadorPage> {
+  final _estiloTexto = new TextStyle(fontSize: 25);
+
+  int _conteo = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Titulo'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Numero de clicks:',
+              style: _estiloTexto,
+            ),
+            Text('$_conteo', style: _estiloTexto)
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          //print('Hola mundo');
+          _conteo++;
+
+          setState(() {});
+        },
+      ),
+    );
+  }
+}
