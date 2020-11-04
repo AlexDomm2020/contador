8ad8eb4caf67405930c84a01f69b0d3a07c33c40
alex ora <alex117a@hotmail.com>
2020-11-04 12:09:27 -0600

    added supr func and 0 initializer fun

--- lib/src/pages/contador_page.dart
+++ lib/src/pages/contador_page.dart
@@ -15,31 +15,58 @@ class _ContadorPageState extends State<ContadorPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Titulo'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Numero de clicks:',
-              style: _estiloTexto,
-            ),
-            Text('$_conteo', style: _estiloTexto)
-          ],
+        appBar: AppBar(
+          title: Text('Titulo'),
+          centerTitle: true,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          //print('Hola mundo');
-          _conteo++;
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Numero de clicks:',
+                style: _estiloTexto,
+              ),
+              Text('$_conteo', style: _estiloTexto)
+            ],
+          ),
+        ),
+        floatingActionButton: _crearBotones());
+  }
 
-          setState(() {});
-        },
-      ),
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30),
+        FloatingActionButton(
+            onPressed: () => _inicializarCero(),
+            child: Icon(Icons.exposure_zero)),
+        Expanded(child: SizedBox()),
+        FloatingActionButton(
+            onPressed: () => _restar(), child: Icon(Icons.remove)),
+        SizedBox(width: 5.0),
+        FloatingActionButton(
+            onPressed: () => _agregar(), child: Icon(Icons.add))
+      ],
     );
+  }
+
+  void _agregar() {
+    setState(() {
+      _conteo++;
+    });
+  }
+
+  void _restar() {
+    setState(() {
+      _conteo--;
+    });
+  }
+
+  void _inicializarCero() {
+    setState(() {
+      _conteo = 0;
+    });
   }
 }
